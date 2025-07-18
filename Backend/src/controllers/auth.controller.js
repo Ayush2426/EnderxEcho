081f556be909bf225bb60c35a0efc99b040b176d
Ayush2426 <av874559@gmail.com>
2025-07-18 23:33:30 +0530

    import resolved

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import User from "../models/user.model.js";
-import { generateToken } from "../lib/Utils.js";
+import generateToken from "../lib/Utils.js"
 import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
