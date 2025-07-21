054823704317170726e4d636c614463c7a02b020
Ayush2426 <av874559@gmail.com>
2025-07-21 15:29:12 +0530

    one m0reeee

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -37,6 +37,34 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+
+console.log("--- Registered Routes ---");
+function print(path, layer) {
+  if (layer.route) {
+    layer.route.stack.forEach(print.bind(null, path.concat(split(layer.route.path))));
+  } else if (layer.name === 'router' && layer.handle.stack) {
+    layer.handle.stack.forEach(print.bind(null, path.concat(split(layer.regexp.source))));
+  } else if (layer.method) {
+    console.log('%s /%s', layer.method.toUpperCase(), path.concat(split(layer.regexp.source)).join('/'));
+  }
+}
+
+function split(thing) {
+  if (typeof thing === 'string') {
+    return thing.split('/');
+  } else if (thing.fast_slash) {
+    return '';
+  } else {
+    var match = thing.toString()
+      .replace('\\/?', '')
+      .replace('(?=\\/|$)', '$')
+      .match(/^\/\^((?:\\[.*+?^${}()|[\]\\\/]|[^.*+?^${}()|[\]\\\/])*)\$\//);
+    return match ? match[1].replace(/\\(.)/g, '$1').split('/') : '<complex:' + thing.toString() + '>';
+  }
+}
+
+
+
 server.listen(PORT || 5001, () => {
   console.log("server is running on PORT:" + PORT);
   connectDB();
