ae83643b331a6ea08a99ca00e7d998f2e69873a0
Ayush2426 <av874559@gmail.com>
2025-07-21 14:50:50 +0530

    a minor change

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,7 +8,7 @@ import ProfilePage from "./Pages/ProfilePage";
 
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useAuthStore } from "./Store/useAuthStore.js";
-import { useThemeStore } from "./store/useThemeStore.js";
+import { useThemeStore } from "./Store/useThemeStore.js";
 import { useEffect } from "react";
 
 import { Loader } from "lucide-react";
